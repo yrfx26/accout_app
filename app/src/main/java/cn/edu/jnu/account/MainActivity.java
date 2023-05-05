bcdfe2911ae767552af9f91a9ebb0470bd86837b
yrfx26 <610263wechan@gmail.com>
2023-05-05 22:05:37 +0800

    bottom navigation 的初步使用

--- app/src/main/java/cn/edu/jnu/account/MainActivity.java
+++ app/src/main/java/cn/edu/jnu/account/MainActivity.java
@@ -1,14 +1,43 @@
 package cn.edu.jnu.account;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        BottomNavigationView nav_view = findViewById(R.id.nav_view);
+        nav_view.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch(id){
+                    case R.id.navigation_item1:
+                        System.out.println("item 1");
+                    case R.id.navigation_item2:
+                        System.out.println("item 2");
+                    case R.id.navigation_item3:
+                        System.out.println("item 3");
+                    //check id
+                }
+                return true;
+            }
+        });
+        System.out.println("start");
+
     }
 }
