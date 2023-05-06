a97ad00dbf4f5d0e2db84a625630aade7e1b14e0
yrfx26 <610263wechan@gmail.com>
2023-05-06 15:38:33 +0800

    bottom navigation 的初步使用

--- app/src/main/java/cn/edu/jnu/account/MainActivity.java
+++ app/src/main/java/cn/edu/jnu/account/MainActivity.java
@@ -33,40 +33,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentManager supportFragmentManager = getSupportFragmentManager();
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        assert navHostFragment != null;
         NavController navController = navHostFragment.getNavController();
         BottomNavigationView bottomNav = findViewById(R.id.nav_view);
         NavigationUI.setupWithNavController(bottomNav, navController);
 
 
-
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-//        BottomNavigationView navigationView = findViewById(R.id.nav_view);
-//        NavigationUI.setupWithNavController(navigationView, navController);
-
-//        FragmentManager supportFragmentManager = getSupportFragmentManager();
-//        NavHostFragment navHostFragment = (NavHostFragment) supportFragmentManager.findFragmentById(R.id.nav_host_fragment);
-//        assert navHostFragment != null;
-//        NavController navController = navHostFragment.getNavController();
-//        assert navController != null;
-//        BottomNavigationView bottomNav = findViewById(R.id.nav_view);
-//        assert bottomNav !=null;
-//        NavigationUI.setupWithNavController(bottomNav, navController);
-//        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
-//            @Override
-//            public void onDestinationChanged(@NonNull NavController navController,
-//                                             @NonNull NavDestination navDestination,
-//                                             @Nullable Bundle bundle) {
-//                switch (navDestination.getId()){
-//                    case R.id.nav_view:
-//                        System.out.println();
-//                        break;
-//                    default:
-//                        System.out.println("ddd");
-//                        break;
-//                }
-//            }
-//        });
     }
 }
