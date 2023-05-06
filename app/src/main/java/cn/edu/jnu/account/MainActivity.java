bbaa841c160bac3c7700fc3bdf21de75051e19b5
yrfx26 <610263wechan@gmail.com>
2023-05-06 14:44:57 +0800

    bottom navigation 的初步使用

--- app/src/main/java/cn/edu/jnu/account/MainActivity.java
+++ app/src/main/java/cn/edu/jnu/account/MainActivity.java
@@ -1,7 +1,17 @@
 package cn.edu.jnu.account;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.NavGraphBuilder;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
@@ -12,38 +22,51 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
 
+
 public class MainActivity extends AppCompatActivity {
-
-
+    AppBarConfiguration appBarConfiguration;
+    private NavController navController;
+    private BottomNavigationView nav_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FragmentManager supportFragmentManager = getSupportFragmentManager();
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNav = findViewById(R.id.nav_view);
+        NavigationUI.setupWithNavController(bottomNav, navController);
 
-        BottomNavigationView nav_view = findViewById(R.id.nav_view);
-        nav_view.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @SuppressLint("NonConstantResourceId")
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
-                switch(id){
-                    case R.id.navigation_item1:
-                        System.out.println("item 1");
-                        break;
-                    case R.id.navigation_item2:
-                        System.out.println("item 2");
-                        break;
-                    case R.id.navigation_item3:
-                        System.out.println("item 3");
-                        break;
-                    //check id
-                }
-                return true;
-            }
-        });
-        Log.i("start", "onCreate: ");
 
+
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//        BottomNavigationView navigationView = findViewById(R.id.nav_view);
+//        NavigationUI.setupWithNavController(navigationView, navController);
+
+//        FragmentManager supportFragmentManager = getSupportFragmentManager();
+//        NavHostFragment navHostFragment = (NavHostFragment) supportFragmentManager.findFragmentById(R.id.nav_host_fragment);
+//        assert navHostFragment != null;
+//        NavController navController = navHostFragment.getNavController();
+//        assert navController != null;
+//        BottomNavigationView bottomNav = findViewById(R.id.nav_view);
+//        assert bottomNav !=null;
+//        NavigationUI.setupWithNavController(bottomNav, navController);
+//        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+//            @Override
+//            public void onDestinationChanged(@NonNull NavController navController,
+//                                             @NonNull NavDestination navDestination,
+//                                             @Nullable Bundle bundle) {
+//                switch (navDestination.getId()){
+//                    case R.id.nav_view:
+//                        System.out.println();
+//                        break;
+//                    default:
+//                        System.out.println("ddd");
+//                        break;
+//                }
+//            }
+//        });
     }
 }
